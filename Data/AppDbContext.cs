d946b5acc521737f5b0d962510ee87d19604e5d5
David Brown <david.brown@efi.com>
2019-12-05 14:54:32 +0000

    Connection string now resolved.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -20,14 +20,26 @@ namespace Warehouse.Models
         public Warehouse.Models.Inventory GetInventory() {
 
             string queryString = "Select * from vision.pub.pv_inventory where pv_inventory.compnum = 413 and pv_inventory.inventoryref = 'ir001'";
-            string connectionString = "";
+            // string connectionString = "DRIVER={Progress OpenEdge 11.7 Driver};HOST=ukdrdev2;PORT=15192;DB=Vision;UID=sysprogress;PWD=sysprogress;DIL=0;AS=50;";
+            string connectionString = "DRIVER={DataDirect 7.1 OpenEdge Wire Protocol};HOST=ukdrdev2;PORT=15192;DB=vision;UID=sysprogress;PWD=sysprogress;DIL=0;AS=50;";
+           // DRIVER ={ DataDirect 7.1 DB2 Wire Protocol}; IpAddress = 123.456.78.90;            PORT = 5179; DB = DB2DATA; UID = JOHN; PWD = XYZZY
             OdbcCommand command = new OdbcCommand(queryString);
+            command.CommandType = System.Data.CommandType.Text;
+            
 
             using (OdbcConnection connection = new OdbcConnection(connectionString))
             {
-                command.Connection = connection;
+                //command.Connection = connection;
                 connection.Open();
-                command.ExecuteNonQuery();
+                //command.ExecuteNonQuery();
+                command.Connection = connection;
+                command.CommandText = queryString;              
+                OdbcDataReader dr = command.ExecuteReader();
+                while(dr.Read())
+                {
+                    string s = dr.ToString();
+                    Console.WriteLine(s);
+                }
 
                 // The connection is automatically closed at 
                 // the end of the Using block.
