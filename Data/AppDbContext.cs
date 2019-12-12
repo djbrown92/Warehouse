7fe71c5989aa7e9eba6e028c55eeccf364f3561a
David Brown <david.brown@efi.com>
2019-12-12 14:30:51 +0000

    ODBC refinements

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -19,36 +19,86 @@ namespace Warehouse.Models
 
         public Warehouse.Models.Inventory GetInventory() {
 
-            string queryString = "Select * from vision.pub.pv_inventory where pv_inventory.compnum = 413 and pv_inventory.inventoryref = 'ir001'";
+            string queryString = "Select * from vision.pub.pv_inventory where pv_inventory.compnum = 413";
             // string connectionString = "DRIVER={Progress OpenEdge 11.7 Driver};HOST=ukdrdev2;PORT=15192;DB=Vision;UID=sysprogress;PWD=sysprogress;DIL=0;AS=50;";
             string connectionString = "DRIVER={DataDirect 7.1 OpenEdge Wire Protocol};HOST=ukdrdev2;PORT=15192;DB=vision;UID=sysprogress;PWD=sysprogress;DIL=0;AS=50;";
            // DRIVER ={ DataDirect 7.1 DB2 Wire Protocol}; IpAddress = 123.456.78.90;            PORT = 5179; DB = DB2DATA; UID = JOHN; PWD = XYZZY
             OdbcCommand command = new OdbcCommand(queryString);
             command.CommandType = System.Data.CommandType.Text;
-            
 
+
+            List<Inventory> inventories = new List<Inventory>();
+
+
+            // this "using" will automatically close the connection
             using (OdbcConnection connection = new OdbcConnection(connectionString))
             {
-                //command.Connection = connection;
-                connection.Open();
-                //command.ExecuteNonQuery();
                 command.Connection = connection;
-                command.CommandText = queryString;              
-                OdbcDataReader dr = command.ExecuteReader();
-                while(dr.Read())
+                connection.Open();
+
+                // System.Data.CommandBehavior cmdBehaviour = System.Data.CommandBehavior.CloseConnection & System.Data.CommandBehavior.SequentialAccess ;
+                OdbcDataReader reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection & System.Data.CommandBehavior.SequentialAccess);
+
+                if (reader.HasRows)
                 {
-                    string s = dr.ToString();
-                    Console.WriteLine(s);
+                    string fields = "";
+
+                    for (int ordinal = 0; ordinal < reader.FieldCount; ordinal++)
+                    {
+                        // You can acess full table metatdata
+                        fields += "field:" + reader.GetName(ordinal) + "; type:" + reader.GetFieldType(ordinal) + "; /n";
+                    }
+
+                    // Populate the Model data   : 
+
+                    while (reader.Read())
+                    {
+                        //Inventory comp = new Company();
+
+                        //comp.id = reader.GetInt32(reader.GetOrdinal("TableRecId")); // get the colum number by name
+
+                        //// lots of ways to access fields
+                        //comp.Name = reader.GetString(0);
+                        //comp.Name = reader["CompName"].ToString();
+
+                        //comp.CompanyNumber = reader.GetInt32(reader.GetOrdinal("CompNum"));
+                        //comp.GroupCode = reader["GroupCode"].ToString();
+
+                        //companies.Add(comp);
+                    }
                 }
+
+                //This frees up the connection as required by the cmd Behaviour setting
+                reader.Close();
 
                 // The connection is automatically closed at 
                 // the end of the Using block.
             }
+
+            //return companies;
+
+            //using (odbcconnection connection = new odbcconnection(connectionstring))
+            //{
+            //    //command.connection = connection;
+            //    connection.open();
+            //    //command.executenonquery();
+            //    command.connection = connection;
+            //    command.commandtext = querystring;              
+            //    odbcdatareader dr = command.executereader();
+            //    while(dr.read())
+            //    {
+            //        string s = dr.tostring();
+            //        console.writeline(s);
+            //    }
+
+            //    // the connection is automatically closed at 
+            //    // the end of the using block.
+            //}
             Warehouse.Models.Inventory inventory = new Warehouse.Models.Inventory();
-            inventory.Compnum = 2;
-            inventory.Inventoryqty = 100;
-            inventory.Inventoryref = "IR0001";
-            inventory.Whousecode = "WH1";
+            //inventory.compnum = 2;
+            //inventory.inventoryqty = 100;
+            //inventory.inventoryref = "ir0001";
+            //inventory.whousecode = "wh1";
             return inventory;
         }
     }
